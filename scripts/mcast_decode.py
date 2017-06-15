247dc3704e76fc2bc6b4661cdf3a4bfe8dad7dcb
Peter Johnson <peter@johnsonhome.org>
2017-06-15 16:00:01 -0500

    updates

--- scripts/mcast_decode.py
+++ scripts/mcast_decode.py
@@ -16,31 +16,18 @@ def decode_lmax(msg):
    pcktime = int(timesec) + int(timenanos)
    return (seqnum,pcktime)
 
-
-def main():
-    parser = argparse.ArgumentParser(description='Subscribe and decode multicast for CME or LMAX')
-    parser.add_argument('-g', '--group',action="append",required=True,help="Group(s) to join in IP:Port format, may be used more than once")
-    parser.add_argument('-i','--interface',required=True,help="IP address of the Interface to join on")
-    parser.add_argument('-d','--decode',required=True,choices=["lmax","cme"],help="LMAX or CME")
-    parser.add_argument('-q',action="count",help="Do not print packet count")
-    args = parser.parse_args()
-
+def join_group(group,args,event):
+    global count
+    (mcast_group,mcast_port) = group.split(":")
+    sock = McastSocket(local_port=int(mcast_port),reuse=1)
+    sock.mcast_add(mcast_group, args.interface)
     stime= datetime.datetime.now()
-    sock = McastSocket(reuse=1)
-    for group in args.group:
-        (mcast_group,mcast_port) = group.split(":")
-        sock.mcast_add(mcast_group, args.interface,int(mcast_port))
-        print "Joining %s:%s at %s" % (mcast_group,mcast_port,stime.strftime("%b %d %Y %X.%f"))
+    print "Joining %s:%s at %s" % (mcast_group,mcast_port,stime.strftime("%b %d %Y %X.%f"))
 
-    ''' Allow Cntl-C to break out of loop '''
-    signal.signal(signal.SIGINT, signal_handler)
-
-    count=0
     MsgSeqNum = 0
-    while True:
+    while not event.isSet():
         msg,source = sock.recvfrom(1500)
-        count = count+1
-
+        count[group] += 1
         if args.decode == "cme":
                 (Num,Time) = decode_cme(msg)
         elif args.decode == "lmax":
@@ -58,11 +45,34 @@ def main():
                 print "Gapped Detected, %s Packets, Sequence Numbers %s-%s at %s" %  (diff-1,MsgSeqNum+1,int(Num)-1,now)
         MsgSeqNum = int(Num)
 
-        if not args.quiet:
-            print "Packets Received: %s" % count ,
-            print '\r',
 
+def main():
+    parser = argparse.ArgumentParser(description='Subscribe and decode multicast for CME or LMAX')
+    parser.add_argument('-g', '--group',action="append",required=True,help="Group(s) to join in IP:Port format, may be used more than once")
+    parser.add_argument('-i','--interface',required=True,help="IP address of the Interface to join on")
+    parser.add_argument('-d','--decode',required=True,choices=["lmax","cme"],help="LMAX or CME")
+    parser.add_argument('-q',action="count",help="Do not print packet count")
+    args = parser.parse_args()
 
+    global estop, count
+    count = {}
+
+    ''' Allow Cntl-C to break out of loop '''
+    signal.signal(signal.SIGINT, signal_handler)
+
+    estop = threading.Event()
+    threads = []
+    for group in args.group:
+        count[group] = 0
+        t = threading.Thread(target=join_group, args=(group,args,estop))
+        threads.append(t)
+        t.start()
+
+    while True:
+        time.sleep(1)
+        for c,v in count.items():
+            print "%s: %s" % (c,v),
+        print "\r",
 
 
 if __name__ == '__main__':
